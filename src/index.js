4628aa954033dab2803f60196befcb5a0feac5f3
Siarhei Labuts <sergeylabut@gmail.com>
2020-02-20 21:52:27 +0300

    fix: 100% test coverage

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
-
-// You should implement your task here.
-
 module.exports = function towelSort (matrix) {
-  return [];
+  if (typeof matrix === 'undefined' || typeof matrix === null || matrix.length === 0) {
+      return [];
+  }  
+  let result = [];
+  for (let i = 0; i < matrix.length; i++) {
+      if (i % 2 === 0) {
+        result.push(matrix[i]);
+      } else {
+        result.push(matrix[i].reverse());
+      }
+      
+  }
+  return result.flat();
 }
